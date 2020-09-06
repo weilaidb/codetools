74a9965e22758fca5d662700d0b592324bc616e5
yjduoduo <wxjlmr@126.com>
2020-09-06 18:49:42 +0800

    移植文件不对

--- codesets/tabwidget.cpp
+++ codesets/tabwidget.cpp
@@ -10,6 +10,7 @@
 #include <ctextcodecpub.h>
 #include <cuipub.h>
 #include "version.h"
+#include "debugApp.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class TabWidget; }
@@ -28,9 +29,6 @@ TabWidget::TabWidget(char *appexe, QWidget *parent)
     CLogPub::msgDefault(appexe); //发现“副本”这两个字的编码是B8B1 B1BE，为GB2312编码，appexe传递的数据为直接为中文编码
     ui->setupUi(this);
     showVersion();
-    initActionSets();
-    initCheckBoxSets();
-    initVars();
     initUiSets();
 
 }
@@ -39,10 +37,7 @@ void TabWidget::showVersion()
 {
     ui->statusbar->showMessage(APP_VERSION);
 }
-void TabWidget::initCheckBoxSets()
-{
 
-}
 
 void TabWidget::initUiSets()
 {
@@ -55,79 +50,6 @@ void TabWidget::initUiSets()
     {
         emit ui->actionnew->triggered();
     }
-}
-
-void TabWidget::initActionSets()
-{
-    QObject::connect(ui->action_codeFormat_File, SIGNAL(triggered()), this, SLOT(proc_action_codeFormat_File()));
-    QObject::connect(ui->action_codeFormat_Directory, SIGNAL(triggered()), this, SLOT(proc_action_codeFormat_Directory()));
-    QObject::connect(ui->action_codeFormat_Edit_Config, SIGNAL(triggered()), this, SLOT(proc_action_codeFormat_Edit_Config()));
-    QObject::connect(ui->action_codeFormat_Save_Config, SIGNAL(triggered()), this, SLOT(proc_action_codeFormat_Save_Config()));
-    QObject::connect(ui->action_codeFormat_Del_Config, SIGNAL(triggered()), this, SLOT(proc_action_codeFormat_Del_Config()));
-    QObject::connect(ui->menu_codeFormat_Recent, SIGNAL(triggered(QAction *)), this, SLOT(proc_menu_codeFormat_Recent(QAction *)));
-    //about
-    QObject::connect(ui->action_about, SIGNAL(triggered()), this, SLOT(proc_action_about()));
-    QObject::connect(ui->action_attention, SIGNAL(triggered()), this, SLOT(proc_action_attention()));
-    //mysql
-    QObject::connect(ui->action_mysql_testdatabase, SIGNAL(triggered()), this, SLOT(proc_action_mysql_testdatabase()));
-
-
-    //office
-    QObject::connect(ui->action_office_open, SIGNAL(triggered()), this, SLOT(proc_action_office_open()));
-    QObject::connect(ui->action_office_search_file, SIGNAL(triggered()), this, SLOT(proc_action_office_search_file()));
-    QObject::connect(ui->action_office_search_dir, SIGNAL(triggered()), this, SLOT(proc_action_office_search_dir()));
-    QObject::connect(ui->menu_document_open_recent, SIGNAL(triggered(QAction *)), this, SLOT(proc_menu_document_open_recent(QAction *)));
-    QObject::connect(ui->menu_document_search_recent, SIGNAL(triggered(QAction *)), this, SLOT(proc_menu_document_search_recent(QAction *)));
-
-    //net
-    QObject::connect(ui->action_net_server, SIGNAL(triggered()), this, SLOT(proc_action_net_server()));
-    QObject::connect(ui->action_net_client, SIGNAL(triggered()), this, SLOT(proc_action_net_client()));
-    QObject::connect(ui->action_net_publish, SIGNAL(triggered()), this, SLOT(proc_action_net_publish()));
-    QObject::connect(ui->action_net_subscribe, SIGNAL(triggered()), this, SLOT(proc_action_net_subscribe()));
-
-    //edit config
-    QObject::connect(ui->action_EditCfgFile, SIGNAL(triggered(bool)), this, SLOT(proc_action_EditCfgFile(bool)));
-    QObject::connect(ui->action_TryAgain, SIGNAL(triggered()), this, SLOT(proc_action_TryAgain()));
-    QObject::connect(ui->action_DeleteCfgFile, SIGNAL(triggered(bool)), this, SLOT(proc_action_DeleteCfgFile(bool)));
-
-//    //text edit
-//    QObject::connect(ui->textEdit, SIGNAL(textChanged()), this, SLOT(proc_textEdit_textChanged()));
-
-    //clipboard change
-
-
-    //check no exist path
-//    QObject::connect(ui->action_checknoexistpath, SIGNAL(triggered(bool)), this, SLOT(proc_action_checknoexistpath(bool)));
-
-    //background update
-    //由于SSD硬盘和机械硬盘检索的差异，反复右键可能在机械硬盘上反应极慢
-    QObject::connect(ui->action_background_update, SIGNAL(triggered(bool)), this, SLOT(proc_action_background_update(bool)));
-    QObject::connect(ui->action_update, SIGNAL(triggered(bool)), this, SLOT(proc_action_update(bool)));
-
-
-}
-
-
-void TabWidget::initVars()
-{
-    openDirPathRecent = "/";
-    openFilePathRecent = "/";
-    openWordFilePathRecent = "/";
-    logAstyleName = "astyle.log";
-    cfgAstyleName = "astyle.conf";
-    cfgAstyleNameOrg = cfgAstyleName + ".org";
-    CStringPub::clearStringList(nameFilters);
-    CStringPub::clearStringList(recentfiles_codeformat);
-    CStringPub::clearStringList(recentfiles_document);
-    m_organization = "weilaidb";
-    m_application = "codesets";
-    m_pSettings = nullptr;
-    m_thread_server = nullptr;
-    m_thread_client = nullptr;
-    m_thread_publish = nullptr;
-    m_thread_subscribe = nullptr;
-
-    CStringPub::clearString(m_EditConfig);
 }
 
 void TabWidget::actNewTab()
--- codesets/tabwidget.h
+++ codesets/tabwidget.h
@@ -19,12 +19,10 @@ class TabWidget : public QMainWindow
 
 public:
     TabWidget(char *appexe, QWidget *parent = nullptr);
-    void initUiSets();
+
 private:
     void showVersion();
-    void initActionSets();
-    void initCheckBoxSets();
-    void initVars();
+    void initUiSets();
 
 public slots:
     void actNewTab();
@@ -35,47 +33,6 @@ private:
     QMdiArea *m_mdiArea;
     quint16 m_tabpos;
     quint16 m_deftabcnt;
-
-private:
-    QString openFilePathRecent;
-    QString openDirPathRecent;
-    char **m_argvp;
-    WORD32 dwArgc;
-    QStringList listAstyleArgv;
-    QString logAstyleName;
-    QString cfgAstyleName;
-    QString cfgAstyleNameOrg;
-
-    QStringList nameFilters;
-    QStringList recentfiles_codeformat;
-    QStringList recentfiles_document;
-
-    QString m_organization;
-    QString m_application;
-
-    QSettings *m_pSettings;
-
-    QString openWordFilePathRecent;
-
-
-    //thread
-    CNetThreadPub *m_thread_server;
-    CNetThreadPub *m_thread_client;
-    CNetThreadPub *m_thread_publish;
-    CNetThreadPub *m_thread_subscribe;
-
-    //Edit File
-    QString m_EditConfig;
-
-    //text edit
-    QTimer *pCheckLeftTimer;
-    int iTimeout;
-    //text edit
-    QTimer *pClipBoardTimer;
-    int iClipBoardTimeout;
-
-    QTimer *pTimerBackgroundUpdate;
-    int iTimeoutBackgroundUpdate;
 
 
 };
