0013363ef59318d1e697f2e4691ed3d875289d67
yjduoduo <wxjlmr@126.com>
2020-09-06 09:58:56 +0800

    默认3个tab

--- codesets/tabwidget.cpp
+++ codesets/tabwidget.cpp
@@ -18,13 +18,25 @@ TabWidget::TabWidget(QWidget *parent)
     : QMainWindow(parent)
     , uitab(new Ui::TabWidget)
     , m_tabpos(0)
-    , m_deftabcnt(5)
+    , m_deftabcnt(3)
 {
     uitab->setupUi(this);
-    connect(uitab->actionnew, SIGNAL(triggered()), this, SLOT(actNewTab()));
 
     initUiSets();
 
+}
+
+void TabWidget::initUiSets()
+{
+    connect(uitab->actionnew, SIGNAL(triggered()), this, SLOT(actNewTab()));
+
+
+    CUIPub::clearTab(uitab->tabWidget);
+    quint16 dwLp =  0;;
+    for(dwLp = 0;dwLp < m_deftabcnt;dwLp++)
+    {
+        emit uitab->actionnew->triggered();
+    }
 }
 
 void TabWidget::actNewTab()
@@ -35,12 +47,3 @@ void TabWidget::actNewTab()
     CUIPub::addTab(uitab->tabWidget, pTabNew, QString("new %1").arg(++m_tabpos));
 }
 
-void TabWidget::initUiSets()
-{
-    CUIPub::clearTab(uitab->tabWidget);
-    quint16 dwLp =  0;;
-    for(dwLp = 0;dwLp < m_deftabcnt;dwLp++)
-    {
-        emit uitab->actionnew->triggered();
-    }
-}
